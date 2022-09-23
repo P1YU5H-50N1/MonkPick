51e3606afdbe0c9723ed1b67d695b63f41a13a1a
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-23 21:01:56 +0530

    Added: Product List to app container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import ProductList from "./component/ProductList";
 import "./index.css";
 
 function App() {
-  return <div className="text-3xl font-bold min-h-screen bg-bg underline text-center">App</div>;
+	return (
+		<div className="bg-bg">
+			<div className="bg-bg container mx-auto pt-20 min-h-screen">
+				<div className="mx-12 md:mx-52">
+					<ProductList />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
