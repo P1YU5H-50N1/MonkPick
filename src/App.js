f40a815fae9400ff342d18b468dba9d75833fcaf
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-23 11:04:10 +0530

    Removed: index.css (Now configured as a build file)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="text-3xl font-bold underline text-center">App</div>;
+  return <div className="text-3xl font-bold min-h-screen bg-bg underline text-center">App</div>;
 }
 
 export default App;
