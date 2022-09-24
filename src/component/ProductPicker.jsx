1adbfd3e4f3ff5bb59b25f990b333e72574aa203
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-24 11:47:01 +0530

    Implemented: Picker with infinite scrolling

--- src/component/ProductPicker.jsx
+++ src/component/ProductPicker.jsx
@@ -1,98 +1,98 @@
 import React from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { useState, useRef, useCallback } from "react";
+import useProductSearch from "./useProductSearch";
+import ProductItem from "./ProductItem";
+const ProductPicker = () => {
+	const [query, setQuery] = useState("");
+	const [pageNum, setPageNum] = useState(1);
 
-const ProductPicker = ({ isOpen, setIsOpen }) => {
-	const closeModal = () => {
-		setIsOpen(false);
+	const handleSearch = (e) => {
+		setQuery(e.target.value);
+		setPageNum(1);
 	};
+
+	const { loading, error, queryProducts, hasMore } = useProductSearch(
+		query,
+		pageNum
+	);
+	console.log("Loading", loading);
+
+	const observer = useRef();
+	const observeContainer = useRef();
+
+	const lastProductRef = useCallback(
+		(node) => {
+			if (loading) return;
+			if (observer.current) observer.current.disconnect();
+			observer.current = new IntersectionObserver(
+				(entries) => {
+					console.log("API Observer", pageNum, hasMore);
+					if (entries[0].isIntersecting) {
+						// console.log("API Increment page num")
+						setPageNum((prevPageNum) => prevPageNum + 1);
+					}
+				},
+				{
+					root: observeContainer.current,
+				}
+			);
+			if (node) observer.current.observe(node);
+			console.log(node);
+		},
+		[loading, hasMore]
+	);
 
 	return (
 		<>
-			<Transition appear show={isOpen} as={Fragment}>
-				<Dialog as="div" className="relative z-10" onClose={closeModal}>
-					<Transition.Child
-						as={Fragment}
-						enter="ease-out duration-300"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="ease-in duration-200"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<div className="fixed inset-0 bg-black bg-opacity-25" />
-					</Transition.Child>
-
-					<div className="fixed inset-0 overflow-y-auto">
-						<div className="flex min-h-full items-center justify-center p-4 text-center">
-							<Transition.Child
-								as={Fragment}
-								enter="ease-out duration-300"
-								enterFrom="opacity-0 scale-95"
-								enterTo="opacity-100 scale-100"
-								leave="ease-in duration-200"
-								leaveFrom="opacity-100 scale-100"
-								leaveTo="opacity-0 scale-95"
-							>
-								<Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-md py-6 bg-white text-left align-middle shadow-xl transition-all">
-									<Dialog.Title
-										as="h3"
-										className="text-lg mx-6 font-medium leading-6 text-gray-900"
-									>
-										Select Products
-									</Dialog.Title>
-
-									<div className="mt-2 px-6 border-t pt-2">
-										<div class="relative text-gray-600 border focus-within:text-gray-400">
-											<span class="absolute inset-y-0 left-0 flex items-center pl-2">
-												<svg
-													fill="none"
-													stroke="currentColor"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													stroke-width="2"
-													viewBox="0 0 24 24"
-													class="w-6 h-6"
-												>
-													<path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-												</svg>
-											</span>
-											<input
-												type="search"
-												name="q"
-												class="py-2 text-sm pl-10 pr-5 w-full focus:outline-none focus:bg-white focus:text-gray-900"
-												placeholder="Search..."
-												autocomplete="off"
-											/>
-										</div>
-									</div>
-
-									<div className="mt-2 text px-6 border-t">
-										Product Item
-									</div>
-
-									<div className="pt-4 px-6 border-t flex justify-end items-center gap-4">
-										<button
-											type="button"
-											className="inline-flex bg-button justify-center rounded-md border border-transparent text-white px-4 py-2 text-sm font-medium"
-											onClick={closeModal}
-										>
-											Cancel
-										</button>
-										<button
-											type="button"
-											className="inline-flex bg-button justify-center rounded-md border border-transparent text-white px-4 py-2 text-sm font-medium"
-											onClick={closeModal}
-										>
-											Add
-										</button>
-									</div>
-								</Dialog.Panel>
-							</Transition.Child>
-						</div>
-					</div>
-				</Dialog>
-			</Transition>
+			<div className="mt-2 px-6 border-t pt-2">
+				<div className="relative text-gray-600 border focus-within:text-gray-400">
+					<span className="absolute inset-y-0 left-0 flex items-center pl-2">
+						<svg
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							viewBox="0 0 24 24"
+							className="w-6 h-6"
+						>
+							<path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+						</svg>
+					</span>
+					<input
+						type="search"
+						name="q"
+						value={query}
+						onChange={handleSearch}
+						className="py-2 text-sm pl-10 pr-5 w-full focus:outline-none focus:bg-white focus:text-gray-900"
+						placeholder="Search..."
+						autoComplete="off"
+					/>
+				</div>
+			</div>
+			<div ref={observeContainer} className="h-[60vh] overflow-y-auto">
+				<div className="mt-2 text px-6 border-t">Product Item</div>
+				{queryProducts.map(({ title, id }, idx) => {
+					return queryProducts.length - 1 === idx ? (
+						<ProductItem
+							lastProductRef={lastProductRef}
+							key={`${id.toString()} ${idx}`}
+							title={title}
+						/>
+					) : (
+						<ProductItem
+							key={`${id.toString()} ${idx}`}
+							title={title}
+						/>
+					);
+				})}
+				{loading ? (
+					<div className="mt-2 text px-6 border-t">Loading...</div>
+				) : null}
+				{error ? (
+					<div className="mt-2 text px-6 border-t">Error...</div>
+				) : null}
+			</div>
 		</>
 	);
 };
