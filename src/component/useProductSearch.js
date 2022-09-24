a42f812e2d6c2665cd7ef106fac93f1c23613caa
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-24 11:45:44 +0530

    Implemented: useProductSearch hook to return product data from API

---
+++ src/component/useProductSearch.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const useProductSearch = (query, pageNum) => {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [queryProducts, setQueryProducts] = useState([]);
+	const [hasMore, setHasMore] = useState(false);
+
+	useEffect(() => {
+		setQueryProducts([]);
+	}, [query]);
+
+	useEffect(() => {
+		setLoading(true);
+		setError(false);
+		let cancel;
+		axios({
+			method: "GET",
+			url: "https://stageapibc.monkcommerce.app/admin/shop/product",
+			params: { search: query, page: pageNum },
+			cancelToken: new axios.CancelToken((c) => (cancel = c)),
+		})
+			.then((res) => {
+				setQueryProducts((prevProducts) => [
+					...prevProducts,
+					...res.data,
+				]);
+				setHasMore(res.data && res.data.length > 0);
+				setLoading(false);
+			})
+			.catch((err) => {
+				if (axios.isCancel(err)) return;
+				setError(true);
+				console.log(err, err.response);
+			});
+
+		return () => cancel();
+	}, [query, pageNum]);
+
+	return { loading, error, queryProducts, hasMore };
+};
+
+export default useProductSearch;
