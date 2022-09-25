5cb96c677682938892d346b3b2bf20f16c928ac5
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-25 17:21:51 +0530

    Wrapped ProductList in provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ProductList from "./component/ProductList";
+import ProductProvider from "./component/ProductProvider";
 import "./index.css";
 
 function App() {
@@ -6,7 +7,9 @@ function App() {
 		<div className="bg-bg">
 			<div className="bg-bg container mx-auto pt-20 min-h-screen">
 				<div className="mx-12 md:mx-52">
-					<ProductList />
+					<ProductProvider>
+						<ProductList />
+					</ProductProvider>
 				</div>
 			</div>
 		</div>
