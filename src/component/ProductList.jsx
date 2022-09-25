a77eaf29e358c8559a0b320908d22bc5e055ebcd
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-25 17:24:13 +0530

    Change: Removed Static data, now it get product data from provider

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -1,58 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import ProductInput from "./ProductInput";
+import { ProductContext } from "./ProductProvider";
 import ProductPickerWrapper from "./ProductPickerWrapper";
 
 const ProductList = () => {
-	const [Products, setProducts] = useState([
-		{
-			id: 77,
-			title: "Fog Linen Chambray Towel - Beige Stripe",
-			variants: [
-				{
-					id: 1,
-					product_id: 77,
-					title: "XS / Silver",
-					price: "49",
-				},
-				{
-					id: 2,
-					product_id: 77,
-					title: "S / Silver",
-					price: "49",
-				},
-				{
-					id: 3,
-					product_id: 77,
-					title: "M / Silver",
-					price: "49",
-				},
-			],
-			image: {
-				id: 266,
-				product_id: 77,
-				src: "https://cdn11.bigcommerce.com/s-p1xcugzp89/products/77/images/266/foglinenbeigestripetowel1b.1647248662.386.513.jpg?c=1",
-			},
-		},
-		{
-			id: 80,
-			title: "Orbit Terrarium - Large",
-			variants: [
-				{
-					id: 64,
-					product_id: 80,
-					title: "Default Title",
-					price: "109",
-				},
-			],
-			image: {
-				id: 272,
-				product_id: 80,
-				src: "https://cdn11.bigcommerce.com/s-p1xcugzp89/products/80/images/272/roundterrariumlarge.1647248662.386.513.jpg?c=1",
-			},
-		},
-	]);
+	// const [Products, setProducts] = useState()
 	const [openPicker, setOpenPicker] = useState(false);
+	const { Products, setProducts } = useContext(ProductContext);
+	// const setProducts = ()=>{console.log("set")}
 	// const onBeforeCapture = () => {
 	// 	/*...*/
 	// };
@@ -78,46 +34,67 @@ const ProductList = () => {
 	};
 
 	return (
-		<div>
-			<ProductPickerWrapper
-				isOpen={openPicker}
-				setIsOpen={setOpenPicker}
-			/>
-			<div className="text-base font-medium">Add Products</div>
+		<>
 			<div>
-				<div className="flex gap-5 mt-4">
-					<div>Product</div>
-					<div>Discount</div>
+				<ProductPickerWrapper
+					isOpen={openPicker}
+					setIsOpen={setOpenPicker}
+				/>
+				<div className="text-base font-medium">Add Products</div>
+				<div>
+					<div className="flex gap-5 mt-4">
+						<div>Product</div>
+						<div>Discount</div>
+					</div>
+					<DragDropContext
+						// onBeforeCapture={onBeforeCapture}
+						// onBeforeDragStart={onBeforeDragStart}
+						// onDragStart={onDragStart}
+						// onDragUpdate={onDragUpdate}
+						onDragEnd={onDragEnd}
+					>
+						<Droppable droppableId="products" type="products">
+							{(provided, snapshot) => (
+								<div
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+								>
+									{Products.map(({ title, id }, idx) => (
+										<ProductInput
+											key={id.toString()}
+											title={title}
+											id={id}
+											openPicker={() =>
+												setOpenPicker(true)
+											}
+											idx={idx}
+										/>
+									))}
+									{provided.placeholder}
+								</div>
+							)}
+						</Droppable>
+					</DragDropContext>
 				</div>
-				<DragDropContext
-					// onBeforeCapture={onBeforeCapture}
-					// onBeforeDragStart={onBeforeDragStart}
-					// onDragStart={onDragStart}
-					// onDragUpdate={onDragUpdate}
-					onDragEnd={onDragEnd}
-				>
-					<Droppable droppableId="products" type="products">
-						{(provided, snapshot) => (
-							<div
-								ref={provided.innerRef}
-								{...provided.droppableProps}
-							>
-								{Products.map(({ title, id }, idx) => (
-									<ProductInput
-										key={id.toString()}
-										title={title}
-										id={id}
-										openPicker={() => setOpenPicker(true)}
-										idx={idx}
-									/>
-								))}
-								{provided.placeholder}
-							</div>
-						)}
-					</Droppable>
-				</DragDropContext>
+
+				{Products.length < 4 ? (
+					<button
+						onClick={() => {
+							setProducts((prevProducts) => [
+								...prevProducts,
+								{
+									title: "",
+									id: "null",
+								},
+							]);
+						}}
+						className="border-button border-4 text-button py-2 px-4 rounded-sm"
+					>
+						Add Product
+					</button>
+				) : null}
 			</div>
-		</div>
+		</>
 	);
 };
 
