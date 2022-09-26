08bd0e5dc3bfa88e4e1f018351ebeb871f6ff0b8
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-26 11:34:26 +0530

    Implemented: Component to show all variants

---
+++ src/component/Variants.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useContext } from "react";
+import { HiChevronDown, HiChevronUp } from "react-icons/hi";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { ProductContext } from "./ProductProvider";
+import Variant from "./Variant";
+
+const Variants = ({ variants, productid }) => {
+	const [Show, setShow] = useState(false);
+    const {reorderVariant,} = useContext(ProductContext);
+
+	const onDragEnd = (result) => {
+        reorderVariant(productid,result.source.index,result.destination.index)
+		// const items = Array.from(Products);
+		// items.splice(
+		// 	result.destination.index,
+		// 	0,
+		// 	...items.splice(result.source.index, 1)
+		// );
+		// setProducts(items);
+	};
+
+	if (variants && variants.length <= 1) {
+		return null;
+	}
+
+	return (
+		<>
+			<button
+				onClick={() => setShow(!Show)}
+				className="flex self-end items-center text-blue-500 underline p-2 mb-2"
+			>
+				{Show ? (
+					<>
+						<p>Hide Variants</p>
+						<HiChevronDown />
+					</>
+				) : (
+					<>
+						<p>Show Variants</p>
+						<HiChevronDown />
+					</>
+				)}
+			</button>
+			{/* <div> */}
+				{Show ? (
+					<DragDropContext
+						// onBeforeCapture={onBeforeCapture}
+						// onBeforeDragStart={onBeforeDragStart}
+						// onDragStart={onDragStart}
+						// onDragUpdate={onDragUpdate}
+						onDragEnd={onDragEnd}
+					>
+						<Droppable droppableId="variants" type="variants">
+							{(provided, snapshot) => (
+								<div
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+									className="flex flex-col gap-4 ml-10"
+								>
+									{variants.map((variant,index) => (
+										<Variant
+											key={variant.admin_graphql_api_id}
+											variant={variant}
+                                            index={index}
+										/>
+									))}
+									{provided.placeholder}
+								</div>
+							)}
+						</Droppable>
+					</DragDropContext>
+				) : null}
+			{/* </div> */}
+		</>
+	);
+};
+
+export default Variants;
