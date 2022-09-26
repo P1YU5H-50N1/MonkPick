bd7eaee10e4b59d0311766ce88f374bf03930bca
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-09-26 17:08:51 +0530

    Minor Fixes

--- src/component/ProductProvider.jsx
+++ src/component/ProductProvider.jsx
@@ -1,89 +1,101 @@
 import { createContext, useContext, useState, useMemo } from "react";
 
-export const ProductContext = createContext({
-  Products: null,
-});
+export const ProductContext = createContext(null);
 
 const ProductProvider = ({ children }) => {
-  const [Products, setProducts] = useState([]);
-  const ProductsLength = Products.filter((p) => p.id !== "null").length;
-  const [PickedProducts, setPickedProducts] = useState([]);
-  const [EditIndex, setEditIndex] = useState(null);
+	const [Products, setProducts] = useState([
+		{
+			title: "",
+			id: "null",
+			variants: [],
+		},
+	]);
+	const ProductsLength = Products.filter((p) => p.id !== "null").length;
+	const [PickedProducts, setPickedProducts] = useState([]);
+	const [EditIndex, setEditIndex] = useState(null);
 
-  const addProducts = (e) => {
-    setProducts((products) => [
-      ...products.slice(0, EditIndex),
-      ...PickedProducts,
-      ...products.slice(EditIndex + 1),
-    ]);
-    setPickedProducts([]);
-  };
+	const addProducts = (e) => {
+		setProducts((products) => [
+			...products.slice(0, EditIndex),
+			...PickedProducts,
+			...products.slice(EditIndex + 1),
+		]);
+		setPickedProducts([]);
+	};
 
-  const removeItem = (idx) => {
-    setProducts((prevProducts) =>
-      prevProducts.filter((product, index) => idx !== index)
-    );
-  };
-  const reorderVariant = (product_id, srcIndex, destIndex) => {
-    setProducts((prevProducts) =>
-      prevProducts.map((product) => {
-        if (product.id === product_id) {
-          const variants = Array.from(product.variants);
-          variants.splice(destIndex, 0, ...variants.splice(srcIndex, 1));
-          console.log(variants);
-          product.variants = variants;
-          return product;
-        } else {
-          return product;
-        }
-      })
-    );
-  };
+	const removeItem = (idx) => {
+		setProducts((prevProducts) =>
+			prevProducts.filter((product, index) => idx !== index)
+		);
+	};
+	const reorderVariant = (product_id, srcIndex, destIndex) => {
+		setProducts((prevProducts) =>
+			prevProducts.map((product) => {
+				if (product.id === product_id) {
+					const variants = Array.from(product.variants);
+					variants.splice(
+						destIndex,
+						0,
+						...variants.splice(srcIndex, 1)
+					);
+					console.log(variants);
+					product.variants = variants;
+					return product;
+				} else {
+					return product;
+				}
+			})
+		);
+	};
 
-  const removeVariant = (product_id, variant_id) => {
-    setProducts((prevProducts) =>
-      prevProducts.map((product) => {
-        if (product.id === product_id) {
-          const variants = Array.from(product.variants);
-          product.variants = variants.filter(({ id }) => id !== variant_id);
-          return product;
-        } else {
-          return product;
-        }
-      })
-    );
-  };
+	const removeVariant = (product_id, variant_id) => {
+		setProducts((prevProducts) =>
+			prevProducts.map((product) => {
+				if (product.id === product_id) {
+					const variants = Array.from(product.variants);
+					product.variants = variants.filter(
+						({ id }) => id !== variant_id
+					);
+					return product;
+				} else {
+					return product;
+				}
+			})
+		);
+	};
 
-  const store = useMemo(
-    () => ({
-      Products,
-      setProducts,
-      disableAddProducts:
-        PickedProducts.length === 0 ||
-        PickedProducts.length + ProductsLength > 4,
-      setPickedProducts,
-      ProductsLength,
-      reorderVariant,
-      removeVariant,
-      removeItem,
-      EditIndex,
-      setEditIndex,
-      addProducts,
-    }),
-    [
-      Products,
-      setProducts,
-      PickedProducts,
-      removeItem,
-      setPickedProducts,
-      EditIndex,
-      setEditIndex,
-    ]
-  );
+	const store = useMemo(
+		() => ({
+			Products,
+			setProducts,
+			disableAddProducts:
+				PickedProducts.length === 0 ||
+				PickedProducts.length + ProductsLength > 4,
+			setPickedProducts,
+			ProductsLength,
+			reorderVariant,
+			removeVariant,
+			removeItem,
+			EditIndex,
+			setEditIndex,
+			addProducts,
+		}),
+		[
+			Products,
+			setProducts,
+			PickedProducts,
+			removeItem,
+			setPickedProducts,
+			EditIndex,
+			setEditIndex,
+		]
+	);
 
-  return (
-    <ProductContext.Provider value={store}>{children}</ProductContext.Provider>
-  );
+	return (
+		<ProductContext.Provider value={store}>
+			{children}
+		</ProductContext.Provider>
+	);
 };
 
 export default ProductProvider;
--- src/component/Variants.jsx
+++ src/component/Variants.jsx
@@ -5,61 +5,65 @@ import { ProductContext } from "./ProductProvider";
 import Variant from "./Variant";
 
 const Variants = ({ variants, productid, discount }) => {
-  const [Show, setShow] = useState(false);
-  const { reorderVariant } = useContext(ProductContext);
+	const [Show, setShow] = useState(false);
+	const { reorderVariant } = useContext(ProductContext);
 
-  const onDragEnd = (result) => {
-    reorderVariant(productid, result.source.index, result.destination.index);
-  };
+	const onDragEnd = (result) => {
+		reorderVariant(
+			productid,
+			result.source.index,
+			result.destination.index
+		);
+	};
 
-  if (variants && variants.length <= 1) {
-    return null;
-  }
+	if (variants && variants.length <= 1) {
+		return null;
+	}
 
-  return (
-    <>
-      <button
-        onClick={() => setShow(!Show)}
-        className="flex self-end items-center text-blue-500 underline p-2 mb-2"
-      >
-        {Show ? (
-          <>
-            <p>Hide Variants</p>
-            <HiChevronDown />
-          </>
-        ) : (
-          <>
-            <p>Show Variants</p>
-            <HiChevronUp />
-          </>
-        )}
-      </button>
-      {Show ? (
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="variants" type="variants">
-            {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className="flex flex-col gap-4 ml-10 pb-4 border-b-2"
-              >
-                {variants.map((variant, index) => (
-                  <Variant
-                    discount={discount}
-                    key={variant.admin_graphql_api_id}
-                    variant={variant}
-                    index={index}
-                  />
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      ) : null}
-      {/* </div> */}
-    </>
-  );
+	return (
+		<>
+			<button
+				onClick={() => setShow(!Show)}
+				className="flex self-end items-center text-blue-500 underline p-2 mb-2"
+			>
+				{Show ? (
+					<>
+						<p>Hide Variants</p>
+						<HiChevronUp />
+					</>
+				) : (
+					<>
+						<p>Show Variants</p>
+						<HiChevronDown />
+					</>
+				)}
+			</button>
+			{Show ? (
+				<DragDropContext onDragEnd={onDragEnd}>
+					<Droppable droppableId="variants" type="variants">
+						{(provided, snapshot) => (
+							<div
+								ref={provided.innerRef}
+								{...provided.droppableProps}
+								className="flex flex-col gap-4 ml-10 pb-4 border-b-2"
+							>
+								{variants.map((variant, index) => (
+									<Variant
+										discount={discount}
+										key={variant.admin_graphql_api_id}
+										variant={variant}
+										index={index}
+									/>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</DragDropContext>
+			) : null}
+			{/* </div> */}
+		</>
+	);
 };
 
 export default Variants;
